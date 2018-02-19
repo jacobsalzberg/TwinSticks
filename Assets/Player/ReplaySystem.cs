a72b0defcbaaf8a6174ac625298701f4f97400c9
Jacob Salzberg <jacob1818@gmail.com>
2018-02-19 04:39:39 -0300

    Add physics system, Mobile Input EventSystem and GameManager

---
+++ Assets/Player/ReplaySystem.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+    private const int bufferFrames = 100; //change replay time
+    private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+    private Rigidbody rigidBody;
+    private GameManager manager;
+
+
+	void Start () {
+        //TEST: 
+        //MyKeyFrame testKey = new MyKeyFrame(1.0f, Vector3.up, Quaternion.identity);
+        //print(testKey.frameTime);
+        rigidBody = GetComponent<Rigidbody>();
+        manager = GameObject.FindObjectOfType<GameManager>();
+	}
+	
+	void Update ()
+    {
+        if (manager.recording)
+        {
+
+            Record();
+        }
+        else
+        {
+            PlayBack();
+        }
+    }
+
+    void PlayBack ()
+    {
+        rigidBody.isKinematic = true;
+        int frame = Time.frameCount % bufferFrames;
+       // print("Reading Frame" + frame);
+        transform.position = keyFrames[frame].position;
+        transform.rotation = keyFrames[frame].rotation;
+    }
+
+    private void Record()
+    {
+        rigidBody.isKinematic = false;
+        int frame = Time.frameCount % bufferFrames;
+        float time = Time.time;
+       // print("Writing frame" + frame);
+        keyFrames[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+    }
+
+}
+
+public struct MyKeyFrame
+{
+    public float frameTime;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public MyKeyFrame (float aTime, Vector3 aPosition, Quaternion aRotation)
+    {
+        frameTime = aTime;
+        position = aPosition;
+        rotation = aRotation;
+    }
+}
---
+++ Assets/PlayerPrefsManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+    //static methods -> make sense as its global information to whole game
+    const string MASTER_VOLUME_KEY = "master_volume";
+    const string DIFFICULTY_KEY = "difficulty";
+    const string LEVEL_KEY = "level_unlocked_";
+    // example: level_unlocked_2, por isso fica o lugar em branco
+
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume >= 0f && volume <= 1f)
+        {
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError("Master volume out of range");
+        }
+    }
+        
+        public static float GetMasterVolume ()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+        
+    }
+
+    public static void UnlockLevel (int level)
+    {
+        if (level <= SceneManager.sceneCountInBuildSettings - 1) //check if there are enough levels (it starts at 0)
+        {
+            PlayerPrefs.SetInt(LEVEL_KEY + level.ToString(),1); // use 1 for true
+        }
+        else
+        {
+            Debug.LogError("Trying to unlock level not in build order");
+        }
+    }
+
+    public static bool IsLevelUnlocked (int level)
+    {
+        int levelValue = PlayerPrefs.GetInt(LEVEL_KEY + level.ToString());
+        bool isLevelUnlocked = (levelValue == 1);
+
+        if (level <= SceneManager.sceneCountInBuildSettings - 1)
+            //return bool
+        {
+            return isLevelUnlocked;
+        }
+        else
+        {
+            Debug.LogError("Trying to query level not in build order");
+            return false;
+        }
+    }
+
+    public static void SetDifficulty (float difficulty)
+    {
+        if (difficulty >= 0f && difficulty <= 3f)
+        {
+            PlayerPrefs.SetFloat(DIFFICULTY_KEY, difficulty);
+        }
+        else
+        {
+            Debug.LogError("Difficulty out of range");
+        }
+    }
+
+    public static float GetDifficulty()
+    {
+        return PlayerPrefs.GetFloat(DIFFICULTY_KEY);
+    }
+}
+
