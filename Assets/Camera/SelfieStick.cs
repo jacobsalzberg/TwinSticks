06aa2f5c166e31463bebc13b838782e9a7199ff6
Jacob Salzberg <jacob1818@gmail.com>
2018-02-19 06:10:00 -0300

    Get Sandbox Ready (Camera OK, TwinStick OK, Input OK)

---
+++ Assets/Camera/SelfieStick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfieStick : MonoBehaviour {
+
+    private GameObject player;
+    private Vector3 armRotation;
+    public float panSpeed = 10f;
+
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        armRotation = transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () { //movement --> update //rendering the results of moving --> late update
+        //print("RHoriz" + Input.GetAxis("RHoriz"));
+        armRotation.z += Input.GetAxis("RVert") * panSpeed;
+        armRotation.y += Input.GetAxis("RHoriz") * panSpeed;
+        transform.position = player.transform.position;
+        transform.rotation = Quaternion.Euler(armRotation);
+	}
+}
--- Assets/Player/GameManager.cs
+++ Assets/Player/GameManager.cs
@@ -6,12 +6,17 @@ using UnityStandardAssets.CrossPlatformInput;
 public class GameManager : MonoBehaviour {
 
     public bool recording = true;
+    private float initialFixedDeltaTime;
+    private bool isPaused = false; 
+
 
     private void Start()
     {
         PlayerPrefsManager.UnlockLevel(2);
         print(PlayerPrefsManager.IsLevelUnlocked(1));
         print(PlayerPrefsManager.IsLevelUnlocked(2));
+        initialFixedDeltaTime = Time.fixedDeltaTime;
+        print(initialFixedDeltaTime);
     }
 
     // Update is called once per frame
@@ -24,6 +29,35 @@ public class GameManager : MonoBehaviour {
         {
             recording = true;
         }
-       
-	}
+
+        if (Input.GetKeyDown(KeyCode.P) && isPaused) //better assign to a remapable button
+        {
+            isPaused = false;
+            ResumeGame(); //update loop still runs, but timescale is set to 0
+        }
+        else if (Input.GetKeyDown(KeyCode.P) && !isPaused) //better assign to a remapable button
+        {
+            isPaused = true;
+            PauseGame(); //update loop still runs, but timescale is set to 0
+        }
+        
+
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        Time.fixedDeltaTime = 0; //physic loops are not called as well
+    }
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = initialFixedDeltaTime;
+    }
+
+    private void OnApplicationPause(bool pauseStatus)
+    {
+        isPaused = pauseStatus;
+    }
+
 }
